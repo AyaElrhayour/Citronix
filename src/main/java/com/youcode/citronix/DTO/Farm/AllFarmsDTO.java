7fafca5e0b4d0c49f12997216fd4c250fca0b27a
Aya <ayaelrhayour123@gmail.com>
2024-11-19 13:10:28 +0100

    created AllFarmDTO

---
+++ src/main/java/com/youcode/citronix/DTO/Farm/AllFarmsDTO.java
@@ -0,0 +1,35 @@
+package com.youcode.citronix.DTO.Farm;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AllFarmsDTO {
+
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String location;
+
+    @Positive
+    @NotNull
+    private Double surface;
+
+    @NotNull
+    private LocalDate creationDate;
+
+    @NotNull
+    private Integer fieldCount;
+}
