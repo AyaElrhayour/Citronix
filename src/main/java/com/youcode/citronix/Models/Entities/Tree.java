c975a2bf663f4034f171639b08ee0fd1d2954edb
Aya <ayaelrhayour123@gmail.com>
2024-11-19 10:15:58 +0100

    created tree entity

---
+++ src/main/java/com/youcode/citronix/Models/Entities/Tree.java
@@ -0,0 +1,36 @@
+package com.youcode.citronix.Models.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "trees")
+public class Tree {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false , name = "age")
+    private int age;
+
+    @Column(name = "plantingDate" , nullable = false)
+    private LocalDate plantingDate;
+
+    @ManyToOne(fetch = FetchType.EAGER , optional = false)
+    private Field field;
+
+    @OneToMany(mappedBy = "arbre", cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+    private Set<HarvestDetails> harvestDetails;
+}
