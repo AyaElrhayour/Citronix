813965da64d2a756c8f75e8f279750ceb02305c4
Aya <ayaelrhayour123@gmail.com>
2024-11-19 15:24:48 +0100

    updated the farm mapper

--- src/main/java/com/youcode/citronix/Mappers/FarmMapper.java
+++ src/main/java/com/youcode/citronix/Mappers/FarmMapper.java
@@ -1,18 +1,29 @@
 package com.youcode.citronix.Mappers;
 
+
 import com.youcode.citronix.DTO.Farm.AllFarmsDTO;
+import com.youcode.citronix.DTO.Farm.FarmCreationDTO;
+import com.youcode.citronix.DTO.Farm.FarmOnlyDTO;
 import com.youcode.citronix.Models.Entities.Farm;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
-public class FarmMapper {
+@Mapper(componentModel = "spring")
+public interface FarmMapper {
 
-    public static AllFarmsDTO toAllFarmsDTO(Farm farm) {
-        return new AllFarmsDTO(
-                farm.getId(),
-                farm.getName(),
-                farm.getLocation(),
-                farm.getSurface(),
-                farm.getCreationDate(),
-                farm.getFields() != null ? farm.getFields().size() : 0
-        );
+    Farm toFarm(FarmCreationDTO farmCreationDTO);
+
+    FarmCreationDTO toFarmCreationDTO(Farm farm);
+
+    AllFarmsDTO toAllFarmsDTO(Farm farm);
+
+    FarmOnlyDTO toFarmOnlyDTO(Farm farm);
+
+    default void calculateFieldsCount(Farm farm, @MappingTarget AllFarmsDTO allFarmsDTO) {
+        if (farm != null && farm.getFields() != null) {
+            allFarmsDTO.setFieldsCount(farm.getFields().size());
+        } else {
+            allFarmsDTO.setFieldsCount(0);
+        }
     }
 }
