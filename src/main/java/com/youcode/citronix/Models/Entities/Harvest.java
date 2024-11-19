b3efd2e03098d67fd861e0a1b28ba386c1970941
Aya <ayaelrhayour123@gmail.com>
2024-11-19 10:16:20 +0100

    created Harvest entity with the enum

---
+++ src/main/java/com/youcode/citronix/Models/Entities/Harvest.java
@@ -0,0 +1,40 @@
+package com.youcode.citronix.Models.Entities;
+
+
+import com.youcode.citronix.Models.Enums.Season;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "harvests")
+public class Harvest {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "harvestDate" , nullable = false)
+    private LocalDate harvestDate;
+
+    @Column(nullable = false , name = "quantiteTotal")
+    private Double quantiteTotal;
+
+    @Column(name = "season" , nullable = false)
+    private Season season;
+
+    @OneToMany(mappedBy = "harvest" , fetch = FetchType.EAGER)
+    private List<Sale> sales;
+
+    @OneToMany(mappedBy = "harvest", cascade = CascadeType.ALL)
+    private List<HarvestDetails> harvestDetails;
+}
---
+++ src/main/java/com/youcode/citronix/Models/Enums/Season.java
@@ -0,0 +1,8 @@
+package com.youcode.citronix.Models.Enums;
+
+public enum Season {
+    FALL,
+    WINTER,
+    SPRING,
+    SUMMER
+}
